e879770cd12075c173cf9c2f70b45504676165a7
omathdevelop <omathdeveloper@gmail.com>
2023-09-21 23:57:09 +0100

    Public diretory updated to ease project deployment

---
+++ Isofi-CMS/public/static/index-0c341ec6-5907a5b3.js
@@ -0,0 +1,1 @@
+import{DocumentPane as r}from"./index-9c45d2a0-a8f69ccc.js";import{D as t}from"./sanity-9b48244e.js";export{t as DocumentPaneProvider,r as default};
--- Isofi-CMS/public/static/index-0c341ec6-f22a4721.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-9c45d2a0-885cd31d.js";import{D as m}from"./sanity-46063217.js";import"./index-5014e344.js";export{m as DocumentPaneProvider,t as default};
