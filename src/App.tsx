4eafc586165218cf32f8167aba17549e2e81bed7
omathdevelop <omathdeveloper@gmail.com>
2023-09-27 21:17:41 +0100

    ProJect Files Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import {Routes, Route} from 'react-router-dom';
+import RootRoute from './routes/@Root/route';
+import PrivacyPolicyRoute from './routes/@PrivacyPolicy/component';
+import TermsAndServiceRoute from './routes/@TermsAndService/component';
+import NaviGation from './components/@Navigation/component';
+const App = () => 
+<Routes>
+    <Route>
+        <Route path={'/'} element={<RootRoute />} />
+        <Route path={'/privacypolicy'} element={<PrivacyPolicyRoute />} />
+        <Route path={'/termsandservice'} element={<TermsAndServiceRoute />} />
 
-
-const App = () =>  <p>isofi coming soon</p>
+    </Route>
+</Routes>
   
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import Store from './context/@redux/store.ts';
 import App from './App.tsx';
-import GlobalView from './design/GlobalView.tsx';
-
+import GlobalView from './design/@GlobalView/component.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider store={Store}>
+    <BrowserRouter future={{v7_startTransition: true}}>
     <GlobalView />
     <App />
+    </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
-)
+);
+
+
