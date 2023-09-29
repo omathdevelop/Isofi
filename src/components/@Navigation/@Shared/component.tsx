1275278a968b11e24a5cca69f7fc02543c2b2a31
omathdevelop <omathdeveloper@gmail.com>
2023-09-29 20:07:44 +0100

    Added a navigation shared component

---
+++ src/components/@Navigation/@Shared/component.tsx
@@ -0,0 +1,11 @@
+import { motion } from 'framer-motion';
+const Menu = () => {
+
+    return (
+        <motion.svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 22 22" fill="none">
+            <path d="M3.79999 6.5H14.15M3.79999 11H9.19999M3.79999 15.5H18.2" stroke="#F8C9C3" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+        </motion.svg>
+    )
+};
+
+export { Menu };
